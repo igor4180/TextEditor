803b86ac312f76f8553cb2a92f11e0a8e59178b4
--- <---.mainry>
2022-11-23 21:26:32 +0300

    123

--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace TextEditor
 {
@@ -35,13 +38,44 @@ namespace TextEditor
         }
         private void main_mi_open_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                if (ofd.ShowDialog() != null)
+                {
 
+                    StreamReader sr = new StreamReader(ofd.OpenFile());
+                    FlowDocument fd = new FlowDocument();
+                    string line;
+                    if (sr != null)
+                    {
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            fd.Blocks.Add(new Paragraph(new Run(line)));
+                        }
+                        rtb_editor.Document = fd;
+                        sr.Close();
+                    }
+                }
+            }
+            catch
+            {
+
+            }
         }
 
 
         private void main_mi_save_Click(object sender, RoutedEventArgs e)
         {
-
+            /*StreamWriter sw = new StreamWriter("document.txt", false, Encoding.UTF8);
+            foreach (Paragraph block in rtb_editor.Document.Blocks)
+            {
+                foreach (Run r in block.Inlines)
+                {
+                    if (r.Text);
+                }
+            }
+            sw.Close();*/
         }
 
         private void main_mi_saveAs_Click(object sender, RoutedEventArgs e)
